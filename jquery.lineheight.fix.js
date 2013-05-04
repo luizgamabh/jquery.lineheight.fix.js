1abce483d08264c6a6354aa9855f52d24c88155d
Luiz Gustavo Freire Gama <luizgamabh@gmail.com>
2013-05-04 16:06:52 -0300

    optimized to change values only on float line-heights

--- jquery.lineheight.fix.js
+++ jquery.lineheight.fix.js
@@ -3,10 +3,14 @@
  * @Author: Luiz Gustavo Freire Gama => http://github.com/luizgamabh
  */
 
+function isFloat(n) {
+    return !isNaN(n) && parseFloat(n) != parseInt(n, 10);
+}
+
 jQuery.fn.lineHeightFix = function() {
     return this.each(function(){
         var lh = jQuery(this).css("line-height").replace(/\s?px/,'');
-        if (!isNaN(lh)) jQuery(this).css("line-height",Math.round(lh)+"px");
+        if (isFloat(lh)) jQuery(this).css("line-height",Math.round(lh)+"px");
     });
 };
 
